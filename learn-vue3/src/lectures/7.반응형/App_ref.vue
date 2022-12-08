ec6b40727bda8f9e83fa168be9bd387fdf0de19c
keyminy <imanaifs@gmail.com>
2022-12-09 00:50:18 +0900

    using computed

---
+++ learn-vue3/src/lectures/7.반응형/App_ref.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<button @click="addMessage">add Click</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		let message = ref('Hello Vue');
+		const addMessage = () => {
+			message.value += '!';
+		};
+		console.log('message : ', message);
+		return {
+			message,
+			addMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectures/7.반응형/App_ref_object_array.vue
@@ -0,0 +1,19 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const message = ref('Hello');
+		const arr = reactive([message]);
+		console.log('arr[0] : ', arr[0].value);
+
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
