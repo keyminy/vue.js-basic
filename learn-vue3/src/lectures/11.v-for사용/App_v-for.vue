9f17025e9d24a25398a3adbe8ecde809c221a197
keyminy <imanaifs@gmail.com>
2022-12-09 17:21:21 +0900

    양방향 바인딩

---
+++ learn-vue3/src/lectures/11.v-for사용/App_v-for.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<ul>
+			<template v-for="(item, idx) in evenItems" :key="item.in">
+				<li>ID : {{ item.id }}, 인덱스 : {{ idx }},아이템 : {{ item.msg }}</li>
+			</template>
+		</ul>
+		<hr />
+		<ul>
+			<li v-for="(val, key, idx) in myObj" :key="key">
+				{{ idx }}-{{ key }}-{{ val }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+	setup() {
+		const items = reactive([
+			{ id: 1, msg: 'Java' },
+			{ id: 2, msg: 'HTML' },
+			{ id: 3, msg: 'CSS' },
+			{ id: 4, msg: 'JS' },
+		]);
+		const evenItems = computed(() => {
+			return items.filter(item => item.id % 2 === 0);
+		});
+		const myObj = reactive({
+			title: '제목',
+			author: '홍길동',
+			pubDate: '2016-01-01',
+		});
+		return {
+			items,
+			evenItems,
+			myObj,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectures/13.이벤트/App_keyboard.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<input type="text" @keyup.enter="addTodo" />
+		<ul>
+			<li v-for="(todo, idx) in todos" :key="idx">
+				{{ todo }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const todos = reactive([]);
+
+		const addTodo = event => {
+			/* Enter를 입력할 때만 입력되게 */
+			todos.push(event.target.value);
+			//enter후 값 비워주기
+			event.target.value = '';
+			event.target.focus(); //focus주기
+		};
+
+		return { todos, addTodo };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
