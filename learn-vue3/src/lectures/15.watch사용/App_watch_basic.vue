ab02e67acef9c66430fadeb2cb77d8b3364f8c48
keyminy <imanaifs@gmail.com>
2022-12-09 18:44:22 +0900

    watch&watchEffect

---
+++ learn-vue3/src/lectures/15.watch사용/App_watch_basic.vue
@@ -0,0 +1,21 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+	setup() {
+		const msg = ref('Hello World');
+		watch(msg, (newValue, oldValue) => {
+			console.log('newValue : ', newValue);
+			console.log('oldValue : ', oldValue);
+		});
+
+		return { msg };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectures/15.watch사용/App_watch활용.vue
@@ -0,0 +1,41 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { watch, ref, reactive } from 'vue';
+
+export default {
+	setup() {
+		const x = ref(0);
+		const y = ref(0);
+
+		const obj = reactive({
+			count: 0,
+		});
+
+		//getter;
+		// watch(
+		// 	() => x.value + y.value,
+		// 	(sum, oldValue) => {
+		// 		console.log(`sum of x + y is: ${sum}`);
+		// 		console.log(`oldValue: ${oldValue}`);
+		// 	},
+		// );
+
+		// watch([x, y], ([newX, newY]) => {
+		// 	console.log('newX : ', newX);
+		// 	console.log('newY : ', newY);
+		// });
+
+		watch(obj, (newVal, oldVal) => {
+			console.log('newValue : ', newVal);
+			console.log('oldVal : ', oldVal);
+		});
+
+		return { x, y, obj };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
